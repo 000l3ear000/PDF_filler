d0732695dc0aa5483cfb5eb72d0688649af97b71
Osama <osamanadeem679@gmail.com>
2021-05-08 17:54:10 +0500

    removed flatten temporarily

--- classes/pdfGenerator.php
+++ classes/pdfGenerator.php
@@ -5,6 +5,7 @@ namespace Classes;
 if ( !defined("ACCESSCHECK") ){
     die("Access not permitted");
 }
+
 use mikehaertl\pdftk\Pdf;
 
 
@@ -13,9 +14,8 @@ class pdfGenerator {
     public function generate($data) {
 
         $filename = 'pdf_' . rand(2000, 1200000) . '.pdf';
-        $pdf = new Pdf('F:\wamp64\www\php\PDF reader\test.pdf');
+        $pdf = new Pdf('test.pdf');
         $res = $pdf->fillForm($data)
-        ->flatten()
         ->saveAs('./completed/' . $filename);
     
         return $filename;
